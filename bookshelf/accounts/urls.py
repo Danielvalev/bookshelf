634a026299ac10f5773f0e6a061ba142c7816235
Daniel Valev <danielvalev89@gmail.com>
2020-12-10 14:06:42 +0200

    [added] register, login and logout cbv

--- bookshelf/accounts/urls.py
+++ bookshelf/accounts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from accounts.views import login_user, logout_user, register_user, user_profile
+from accounts.views import user_profile, LogoutView, LoginView, RegisterView
 
 urlpatterns = [
-    path('login/', login_user, name='login user'),
-    path('logout/', logout_user, name='logout user'),
-    path('register/', register_user, name='register user'),
+    # path('login/', login_user, name='login user'),
+    path('login/', LoginView.as_view(), name='login user'),  # CBV
+    # path('logout/', logout_user, name='logout user'),
+    path('logout/', LogoutView.as_view(), name='logout user'),  # CBV
+    # path('register/', register_user, name='register user'),
+    path('register/', RegisterView.as_view(), name='register user'), # CBV
     # path('profile/', user_profile, name='current user profile'),
     path('profile/<int:pk>', user_profile, name='user profile'),
 
--- bookshelf/accounts/views.py
+++ bookshelf/accounts/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import views as auth_views
+from django.views import generic as views
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db import transaction
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from accounts.models import UserProfile
 from books.models import Book
 from accounts.forms import RegisterForm, ProfileForm, LoginForm
 
@@ -10,75 +15,102 @@ from accounts.forms import RegisterForm, ProfileForm, LoginForm
 # Create your views here.
 
 
-@transaction.atomic
-def register_user(request):
-    if request.method == 'GET':
-        context = {
-            'user_form': RegisterForm(),
-            'profile_form': ProfileForm(),
-
-        }
-        return render(request, 'accounts/register.html', context)
-    else:  # POST
-        user_form = RegisterForm(request.POST)
-        profile_form = ProfileForm(request.POST, request.FILES)  # request.FILES is mandatory for MEDIA
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            login(request, user)  # login the user
-            return redirect('index')
-
-        context = {
-            'user_form': RegisterForm(),
-            'profile_form': ProfileForm(),
-        }
-
-        return render(request, 'accounts/register.html', context)
-
-
-def get_redirect_url(params):
-    redirect_url = params.get('return_url')
-    if not redirect_url:
-        return 'index'
-    return redirect_url
+# @transaction.atomic
+# def register_user(request):
+#     if request.method == 'GET':
+#         context = {
+#             'form': RegisterForm(),
+#             'profile_form': ProfileForm(),
+#
+#         }
+#         return render(request, 'accounts/register.html', context)
+#     else:  # POST
+#         user_form = RegisterForm(request.POST)
+#         profile_form = ProfileForm(request.POST, request.FILES)  # request.FILES is mandatory for MEDIA
+#
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user = user_form.save()
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#             profile.save()
+#             login(request, user)  # login the user
+#             return redirect('index')
+#
+#         context = {
+#             'form': RegisterForm(),
+#             'profile_form': ProfileForm(),
+#         }
+#
+#         return render(request, 'accounts/register.html', context)
 
 
-def login_user(request):
-    if request.method == 'GET':
-        login_form = LoginForm()
+class RegisterView(views.CreateView):
+    template_name = 'accounts/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('index')
 
-        context = {
-            'login_form': login_form,
-        }
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        user = form.save()
 
-        return render(request, 'accounts/login.html', context)
-    else:
-        login_form = LoginForm(request.POST)
-        return_url = get_redirect_url(request.POST)
+        # Create profile
+        profile = UserProfile(user=user)
+        profile.save()
 
-        if login_form.is_valid():
-            username = login_form.cleaned_data['username']
-            password = login_form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-
-            if user:
-                login(request, user)
-                return redirect(return_url)
-
-        context = {
-            'login_form': login_form,
-        }
-
-        return render(request, 'accounts/login.html', context)
+        login(self.request, user)
+        return valid
 
 
-@login_required
-def logout_user(request):
-    logout(request)
-    return redirect('index')
+# Used in login_user
+# def get_redirect_url(params):
+#     redirect_url = params.get('return_url')
+#     if not redirect_url:
+#         return 'index'
+#     return redirect_url
+
+
+# def login_user(request):
+#     if request.method == 'GET':
+#         login_form = LoginForm()
+#
+#         context = {
+#             'form': login_form,
+#         }
+#
+#         return render(request, 'accounts/login.html', context)
+#     else:
+#         login_form = LoginForm(request.POST)
+#         return_url = get_redirect_url(request.POST)
+#
+#         if login_form.is_valid():
+#             username = login_form.cleaned_data['username']
+#             password = login_form.cleaned_data['password']
+#             user = authenticate(username=username, password=password)
+#
+#             if user:
+#                 login(request, user)
+#                 return redirect(return_url)
+#
+#         context = {
+#             'form': login_form,
+#         }
+#
+#         return render(request, 'accounts/login.html', context)
+
+# Login CBV
+class LoginView(auth_views.LoginView):
+    template_name = 'accounts/login.html'
+
+
+# @login_required
+# def logout_user(request):
+#     logout(request)
+#     return redirect('index')
+
+
+# CBV
+class LogoutView(auth_views.LogoutView):
+    next_page = reverse_lazy('index')
 
 
 def user_profile(request, pk=None):
